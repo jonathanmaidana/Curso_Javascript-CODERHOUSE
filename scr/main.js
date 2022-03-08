6f327551a7a0e74d283db5f1fdfd1b805bc09822
Jonathan <jonathanmaidana24@gmail.com>
2022-03-07 21:56:12 -0300

    estructura de pruducto html, simulador de descuento e iva a un producto

--- scr/main.js
+++ scr/main.js
@@ -1,6 +1,19 @@
-let numero = parseInt (prompt(''));
+function encargo () {
+    let gustoDeHelado = prompt('Gusto de Helado')
+    let cantidadDeHelado = prompt('¿Cuantos queres?')
+    const suma = (a,b) => a + b
+    const resta = (a,b) => a - b
+    const iva = x => x * 0.10
+    let precioNumero1 = 300
+    let precioFinal = (suma(precioNumero1, iva(precioNumero1))) * cantidadDeHelado
+    let primerDescuento = 100
+    let segundoDescuento = 250
 
-for (let i = 1; i <= 10; i++) {
-    let resultado = numero + i;
-    alert(numero + '+' + i + '=' + resultado);
+    if ((precioFinal > 900) && (precioFinal <= 1500)){
+        alert('Tu pedido: ' + gustoDeHelado + ', ' + 'cantidad ' + cantidadDeHelado + ', ' + 'Precio Final con un descuento de $100 es de ' + (precioFinal - primerDescuento));
+    } else if ((precioFinal > 1500) && (precioFinal => 2500)){
+        alert('Tu pedido: ' + gustoDeHelado + ', ' + ',cantidad ' + cantidadDeHelado + ', ' + 'Precio Final con un descuento de $250 es de ' + (precioFinal - segundoDescuento));
+    } else{
+        alert('Tu pedido: ' + gustoDeHelado + ', ' + 'cantidad ' + cantidadDeHelado + ', ' + 'Precio Final ' + precioFinal);
+    }
 }
